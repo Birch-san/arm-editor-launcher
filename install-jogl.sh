716ca9cf7e6c7af9218eb78f0ed9707ce12b2920
Alex Birch <Birch-san@users.noreply.github.com>
2022-02-19 17:40:24 +0000

    navigate to directory which lacks a pom, before we install jogl

--- install-jogl.sh
+++ install-jogl.sh
@@ -37,11 +37,12 @@ JAR_DIR="$ARCHIVE/jar"
 curl -o jogamp-rc4.zip "https://download.jzy3d.org/jogl/$ARCHIVE.zip"
 unzip jogamp-rc4.zip "$JAR_DIR/*" -d jogamp-rc4
 
-LIB="$OUT/$JAR_DIR"
+# leave our root directory, because the presence of its pom.xml causes install:install-file to do more than we intended
+cd "$OUT"
 
 version="v2.4.0-rc4"
 
-"$MVN" install:install-file -DgroupId=org.jogamp.gluegen -DartifactId=gluegen-rt                                       -Dversion="$version" -Dfile="${LIB}/gluegen-rt.jar"                          -Dpackaging=jar
-"$MVN" install:install-file -DgroupId=org.jogamp.gluegen -DartifactId=gluegen-rt -Dclassifier=natives-macosx-universal -Dversion="$version" -Dfile="${LIB}/gluegen-rt-natives-macosx-universal.jar" -Dpackaging=jar
-"$MVN" install:install-file -DgroupId=org.jogamp.jogl -DartifactId=jogl-all                                            -Dversion="$version" -Dfile="${LIB}/jogl-all.jar"                            -Dpackaging=jar
-"$MVN" install:install-file -DgroupId=org.jogamp.jogl -DartifactId=jogl-all      -Dclassifier=natives-macosx-universal -Dversion="$version" -Dfile="${LIB}/jogl-all-natives-macosx-universal.jar"   -Dpackaging=jar
+"$MVN" install:install-file -DgroupId=org.jogamp.gluegen -DartifactId=gluegen-rt                                       -Dversion="$version" -Dfile="${JAR_DIR}/gluegen-rt.jar"                          -Dpackaging=jar
+"$MVN" install:install-file -DgroupId=org.jogamp.gluegen -DartifactId=gluegen-rt -Dclassifier=natives-macosx-universal -Dversion="$version" -Dfile="${JAR_DIR}/gluegen-rt-natives-macosx-universal.jar" -Dpackaging=jar
+"$MVN" install:install-file -DgroupId=org.jogamp.jogl -DartifactId=jogl-all                                            -Dversion="$version" -Dfile="${JAR_DIR}/jogl-all.jar"                            -Dpackaging=jar
+"$MVN" install:install-file -DgroupId=org.jogamp.jogl -DartifactId=jogl-all      -Dclassifier=natives-macosx-universal -Dversion="$version" -Dfile="${JAR_DIR}/jogl-all-natives-macosx-universal.jar"   -Dpackaging=jar
